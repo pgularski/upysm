90364d104731130386d2577aa8b6b705cc2fc4d3
Piotr Gularski <piotr.gularski@gmail.com>
2019-01-02 18:11:14 +0100

    bdist_wheel is a no no for upython

--- tasks.py
+++ tasks.py
@@ -22,7 +22,7 @@ def build(c):
 @task(copy_pysm_code)
 def sdist(c):
     c.run("python setup.py sdist")
-    c.run("python setup.py bdist_wheel")
+    #  c.run("python setup.py bdist_wheel")
     c.run("rm dist/*.orig")
 
 
